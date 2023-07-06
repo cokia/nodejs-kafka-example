d4cc9ff6989c6dc9ad6762f5c6600ed0b5dd3c72
cokia <hanu@hanukoon.com>
2023-07-06 16:11:09 +0900

    feat: implement producer

--- config.ts
+++ config.ts
@@ -1,8 +1,6 @@
 require('dotenv').config({ path: `${__dirname}/.env` });
 
 export default {
-  queueUrl: process.env.queueUrl || 'https://sqs.ap-northeast-2.amazonaws.com/account-id/queue-name',
-  region: process.env.region || 'ap-northeast-2',
-  accessKeyId: process.env.aws_access_key_id || '',
-  secretAccessKey: process.env.aws_secret_access_key || ''
+  clientId: process.env.CLIENT_ID || 'nodejs-example-client-app',
+  brokers: process.env.BROKERS || 'localhost:9092',
 }
--- producer.ts
+++ producer.ts
@@ -1,84 +1,33 @@
-import { Producer } from 'sqs-producer';
-import config from './config'
-import crypto from 'crypto'
+import { Kafka } from "kafkajs";
+import config from "./config";
 
-function timestamp() {
-  var today = new Date();
-  today.setHours(today.getHours() + 9);
-  return today.toISOString().replace('T', ' ').substring(0, 19);
+const kafka = new Kafka({
+  clientId: config.clientId,
+  brokers: config.brokers.split(","),
+});
+
+const producer = kafka.producer();
+
+const initKafkaConnection = async () => {
+  return await producer.connect();
 }
 
-const exampleProducerFunctionForStandardQueue = async () => {
-  // create custom producer (supporting all opts as per the API docs: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/SQS.html#constructor-property)
-  const producer = Producer.create({
-    queueUrl: config.queueUrl,
-    region: config.region
-  });
+// 만약 토픽이 존재하지 않을 경우, 토픽을 생성하기 위해서는 카프카에서 auto.create.topics.enable 설정을 true로 하여야 한다.
 
-  // send simple messages to the queue
-  await producer.send(['msg1', 'msg2']);
+const sendMessage = async (topic: string, message: string) => {
+  try {
+    await producer.send({
+      topic,
+      messages: [
+        { value: message },
+      ],
+    });
+  } catch (error) {
+    console.error(`[KafkaSendErr]${error}`);
 
-  // send a message to the queue with a specific ID (by default the body is used as the ID)
-  await producer.send([{
-    id: 'id1',
-    body: 'Hello Standard Queue!' + timestamp()
-  }]);
-
-  // send a message to the queue with
-  // - delaySeconds (must be an number contained within 0 and 900)
-  // - messageAttributes
-  await producer.send([
-    {
-      id: 'id1',
-      body: 'Hello world with two string attributes: attr1 and attr2',
-      messageAttributes: {
-        attr1: { DataType: 'String', StringValue: 'stringValue' },
-        attr2: { DataType: 'Binary', BinaryValue: new Buffer('binaryValue') }
-      }
-    },
-    {
-      id: 'id2',
-      body: 'Hello world delayed by 5 seconds',
-      delaySeconds: 5
-    }
-  ]);
-  console.log(`[${timestamp()}] successfully sent message to FIFO queue!`)
-
-  // get the current size of the queue
-  const size = await producer.queueSize();
-  console.log(`There are ${size} messages on the queue.`);
+  }
+  return true
 }
 
-const exampleProducerFunctionForFIFOQueue = async () => {
-  // create custom producer (supporting all opts as per the API docs: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/SQS.html#constructor-property)
-  const producer = Producer.create({
-    queueUrl: config.queueUrl,
-    region: config.region
-  });
+initKafkaConnection();
 
-  // send a message to a FIFO queue
-  //
-  // note that AWS FIFO queues require two additional params:
-  // - groupId (string)
-  // - deduplicationId (string)
-  //
-  // deduplicationId can be excluded if content-based deduplication is enabled
-  //
-  // http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/FIFO-queue-recommendations.html
-  const body = 'Hello FIFO queue! ' + timestamp()
-  await producer.send({
-    id: "testId",
-    body,
-    groupId: 'group1234',
-    // generate md5 hash of body
-    deduplicationId: crypto.createHash('md5').update(body).digest('hex')
-  });
-
-  console.log(`[${timestamp()}] successfully sent message to FIFO queue!`)
-
-  const size = await producer.queueSize();
-  console.log(`There are ${size} messages on the queue.`);
-}
-
-exampleProducerFunctionForFIFOQueue()
-// exampleProducerFunctionForStandardQueue()
